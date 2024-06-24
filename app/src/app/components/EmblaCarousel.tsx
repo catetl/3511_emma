d898ac5785556f05ac5f364ee23f95d641bcc302
emmalum <emmalum20@gmail.com>
2024-06-24 15:11:32 +1000

    typescript

--- app/src/app/components/EmblaCarousel.tsx
+++ app/src/app/components/EmblaCarousel.tsx
@@ -1,23 +1,32 @@
 import React, { useCallback, useEffect, useRef } from 'react'
+import {
+  EmblaCarouselType,
+  EmblaEventType,
+  EmblaOptionsType
+} from 'embla-carousel'
 import useEmblaCarousel from 'embla-carousel-react'
 import {
   NextButton,
   PrevButton,
   usePrevNextButtons
 } from './CarouselArrowButton'
-
 import { DotButton, useDotButton } from './CarouselDotButton'
 
 const TWEEN_FACTOR_BASE = 0.52
 
-const numberWithinRange = (number, min, max) =>
+const numberWithinRange = (number: number, min: number, max: number): number =>
   Math.min(Math.max(number, min), max)
 
-const EmblaCarousel = (props) => {
+type PropType = {
+  slides: number[]
+  options?: EmblaOptionsType
+}
+
+const EmblaCarousel: React.FC<PropType> = (props) => {
   const { slides, options } = props
   const [emblaRef, emblaApi] = useEmblaCarousel(options)
   const tweenFactor = useRef(0)
-  const tweenNodes = useRef([])
+  const tweenNodes = useRef<HTMLElement[]>([])
 
   const { selectedIndex, scrollSnaps, onDotButtonClick } =
     useDotButton(emblaApi)
@@ -29,53 +38,56 @@ const EmblaCarousel = (props) => {
     onNextButtonClick
   } = usePrevNextButtons(emblaApi)
 
-  const setTweenNodes = useCallback((emblaApi) => {
+  const setTweenNodes = useCallback((emblaApi: EmblaCarouselType): void => {
     tweenNodes.current = emblaApi.slideNodes().map((slideNode) => {
-      return slideNode.querySelector('.embla__slide__number')
+      return slideNode.querySelector('.embla__slide__number') as HTMLElement
     })
   }, [])
 
-  const setTweenFactor = useCallback((emblaApi) => {
+  const setTweenFactor = useCallback((emblaApi: EmblaCarouselType) => {
     tweenFactor.current = TWEEN_FACTOR_BASE * emblaApi.scrollSnapList().length
   }, [])
 
-  const tweenScale = useCallback((emblaApi, eventName) => {
-    const engine = emblaApi.internalEngine()
-    const scrollProgress = emblaApi.scrollProgress()
-    const slidesInView = emblaApi.slidesInView()
-    const isScrollEvent = eventName === 'scroll'
+  const tweenScale = useCallback(
+    (emblaApi: EmblaCarouselType, eventName?: EmblaEventType) => {
+      const engine = emblaApi.internalEngine()
+      const scrollProgress = emblaApi.scrollProgress()
+      const slidesInView = emblaApi.slidesInView()
+      const isScrollEvent = eventName === 'scroll'
 
-    emblaApi.scrollSnapList().forEach((scrollSnap, snapIndex) => {
-      let diffToTarget = scrollSnap - scrollProgress
-      const slidesInSnap = engine.slideRegistry[snapIndex]
+      emblaApi.scrollSnapList().forEach((scrollSnap, snapIndex) => {
+        let diffToTarget = scrollSnap - scrollProgress
+        const slidesInSnap = engine.slideRegistry[snapIndex]
 
-      slidesInSnap.forEach((slideIndex) => {
-        if (isScrollEvent && !slidesInView.includes(slideIndex)) return
+        slidesInSnap.forEach((slideIndex) => {
+          if (isScrollEvent && !slidesInView.includes(slideIndex)) return
 
-        if (engine.options.loop) {
-          engine.slideLooper.loopPoints.forEach((loopItem) => {
-            const target = loopItem.target()
+          if (engine.options.loop) {
+            engine.slideLooper.loopPoints.forEach((loopItem) => {
+              const target = loopItem.target()
 
-            if (slideIndex === loopItem.index && target !== 0) {
-              const sign = Math.sign(target)
+              if (slideIndex === loopItem.index && target !== 0) {
+                const sign = Math.sign(target)
 
-              if (sign === -1) {
-                diffToTarget = scrollSnap - (1 + scrollProgress)
+                if (sign === -1) {
+                  diffToTarget = scrollSnap - (1 + scrollProgress)
+                }
+                if (sign === 1) {
+                  diffToTarget = scrollSnap + (1 - scrollProgress)
+                }
               }
-              if (sign === 1) {
-                diffToTarget = scrollSnap + (1 - scrollProgress)
-              }
-            }
-          })
-        }
+            })
+          }
 
-        const tweenValue = 1 - Math.abs(diffToTarget * tweenFactor.current)
-        const scale = numberWithinRange(tweenValue, 0, 1).toString()
-        const tweenNode = tweenNodes.current[slideIndex]
-        tweenNode.style.transform = `scale(${scale})`
+          const tweenValue = 1 - Math.abs(diffToTarget * tweenFactor.current)
+          const scale = numberWithinRange(tweenValue, 0, 1).toString()
+          const tweenNode = tweenNodes.current[slideIndex]
+          tweenNode.style.transform = `scale(${scale})`
+        })
       })
-    })
-  }, [])
+    },
+    []
+  )
 
   useEffect(() => {
     if (!emblaApi) return
