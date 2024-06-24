6fc76a5e6937ec72a1dc8f0b359596b73ba663e6
Cate <cate@Cates-MacBook-Air.local>
2024-06-24 14:25:37 +1000

    tester

---
+++ app/src/app/page.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+
+// need to create style constants e.g. fonts, colours
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      <header className="fixed top-0 left-0 right-0 z-50 bg-white shadow-md py-4 px-8 flex items-center justify-between">
+        <div>
+          <h1>UNSW TerriaJS TESTER</h1>
+        </div>
+        <nav className="flex space-x-6">
+          <a href="#what-we-offer" className="text-gray-600 hover:text-gray-900">
+            What We Offer
+          </a>
+          <a href="#tutorials" className="text-gray-600 hover:text-gray-900">
+            Tutorials
+          </a>
+          <a href="#news" className="text-gray-600 hover:text-gray-900">
+            News
+          </a>
+          <a href="#team" className="text-gray-600 hover:text-gray-900">
+            Our Team
+          </a>
+          <a href="#team" className="text-gray-600 hover:text-gray-900">
+            Feedback
+          </a>
+        </nav>
+
+
+        <div className="space-x-4">
+          <button className="bg-lightgrey hover:bg-gray-900 text-white px-4 py-2 rounded-md">
+            Login
+          </button>
+          <button className="bg-darkgrey hover:bg-gray-900 text-white px-4 py-2 rounded-md">
+            Register
+          </button>
+        </div>
+      </header>
+
+      <main className="bg-white flex pt-20">
+        <section className="container mx-auto px-4">
+          <h1>
+            FJSALFJLDJFKL
+          </h1>
+          <h1>
+            fdklsjflkd;jflkdjalfjdl
+          </h1>
+          <div className="bg-gray-200 h-40 flex items-center justify-center">
+            Box 1
+          </div>
+          <div className="bg-gray-300 h-40 flex items-center justify-center">
+            Box 2
+          </div>
+          <div className="bg-gray-400 h-40 flex items-center justify-center">
+            Box 3
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+};
